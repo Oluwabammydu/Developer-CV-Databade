fe0b39e7ed38298b0ed100114c411a46012a4ba4
Oluwabammydu <oluwabamise77@gmail.com>
2018-10-30 16:00:09 +0100

    Updating

--- app.py
+++ app.py
@@ -21,13 +21,9 @@ def home():
 		query = "INSERT INTO developers_profile(name, skills, github_link, file) VALUES('{}', '{}', '{}', '{}')".format(name, skills, github_link, cv_upload)
 		cur.execute(query)
 		conn.commit()
-		#if query 
-		#flash('Your Profile have been saved, thanks for using Sammler')
+		flash('Your Profile have been saved, thanks for using Sammler')
 		return redirect(url_for('status')) 
-		'''
-		except Exception as e:
-			return "Database connection error", print(e)
-		'''
+		
 	return render_template('home.html')
 
 
@@ -64,51 +60,12 @@ def to_search():
 		
 
 		return render_template("search.html", rows=rows)
-	'''
-	results = []
-	searc_string = request.form['search']
-	if search.data['search'] == '':
-		qr = conn.query(developers_profile)
-		results = qry.all()
-	if not results:
-		flash('No results found!')
-		return redirect('/search')
-	else:
-		#display results
-		return render_template('search.html', results=results)
-	
-	-----------------
-	results = []
-	search_string = search.data['search']
-	if search.data['search'] == '':
-		qry = db_session.query(developers_profile)
-		results = qry.all()
-	if not results:
-		flash('No results found!')
-		return redirect('/')
-	else:
-		# display results
-		return render_template('search.html', results=results)
-	-----------------
-	'''
-
-   # query = request.args('search')
-# Query table
-#for row in conn.execute('SELECT * FROM cafe'):
- #   print(row)
-
-# give connection back to the connection pool
-#conn.close()
-	
-
-
 
 @app.route("/status", methods=['GET', 'POST'])
 def status():
 	return render_template('status.html')
-
-
-
+	
+	
 
 
 
