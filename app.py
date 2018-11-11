7b5cd7bcc78e14ac63b644fb6a0a6e9513ab87db
Oluwabammydu <oluwabamise77@gmail.com>
2018-11-11 12:46:27 +0100

    Updating

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
 from flask import Flask, render_template, url_for, request, redirect, flash
 import mysql.connector as mariadb
+import os
+import pymysql
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+
+port = int(os.environ.get("PORT", 3306))
+
+app.config['MYSQL_HOST'] = 'pfw0ltdr46khxib3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'
+app.config['MYSQL_USER'] = 'zqcqd8wm0u73wqym'
+app.config['MYSQL_PASSWORD'] = 'ccysgm4bx7jhzgug'
+app.config['MYSQL_DB'] = 'nady2nvsj52l50go'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+mysql = MySQL(app)
 
 
 
@@ -13,13 +26,17 @@ def home():
 		skills = request.form['skills']
 		github_link = request.form['githublink']
 		cv_upload = request.form['file']
-		conn = mariadb.connect(user='root',
-							   password='bammy', 
-							   database='sammler')
-		cur = conn.cursor()
-		query = "INSERT INTO developers_profile(name, skills, github_link, file) VALUES('{}', '{}', '{}', '{}')".format(name, skills, github_link, cv_upload)
-		cur.execute(query)
-		conn.commit()
+
+		# Create cursor
+		cursor = mysql.connection.cursor()
+		cursor.execute("INSERT INTO developers_profile(name, skills, github_link, file) VALUES('{}', '{}', '{}', '{}')".format(name, skills, github_link, cv_upload))
+		
+		# Commit to database
+		mysql.connection.commit()
+
+		# Close connection
+		cursor.close()
+
 		#flash('Your Profile have been saved, thanks for using Sammler')
 		return redirect(url_for('status')) 
 		
@@ -43,32 +60,27 @@ def to_search():
 
 		search = request.form['search']
 
-		conn = mariadb.connect(user='root',
-								   password='bammy', 
-								   database='sammler')
-		cur = conn.cursor()
-		query = "SELECT name,github_link FROM developers_profile WHERE skills LIKE %s"
-		search = ('%'+search+'%', )
-		cur.execute(query, search)
-		rows = cur.fetchall()
-		print(rows)
-		cur.close()
-		conn.close()
-	#if rows not:
-		
+		# Create cursor
+		cursor = mysql.connection.cursor()
+
+		# Get developer by search 
+		result = cursor.execute("SELECT * FROM developers_profile WHERE skills = %s", [search])
+
+		rows = cursor.fetchall()
+
 		return render_template("search.html", rows=rows)
+				
+		cursor.close()
+		
 
-
+		
+		
 @app.route("/status", methods=['GET', 'POST'])
 def status():
 	return render_template('status.html')
 	
 	
 
-
-
-
-
 if __name__ == '__main__':
 	app.secret_key='secret_key'
-	app.run(debug=True)
+	app.run(debug=True, host='0.0.0.0', port=port)
